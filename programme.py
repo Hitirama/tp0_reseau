ce18a35957959f362dc4e2ccea42adb6e26ea66a
Hitirama Snow <h.snow@saint-louis29.net>
2024-02-13 13:34:08 +0100

    ex 4 en cours

--- programme.py
+++ programme.py
@@ -30,7 +30,52 @@ assert get_int_ip('192.168.1.40') == 3232235816
 get_int_ip('192.168.1.40')
 
 def get_pointed_mask(cidr):
-    return get_pointed_ip((2**cidr-1)<<(32-cidr))
+    """
+    paramètre : ip_cidr une addresse ip décimale pointée avec son cidr exemple 192.168.1.40/24
+    Retourne, sous forme d'adresse IP pointée, l'adresse du réseau
+    """
+    return get_pointed_ip((2**cidr-1)<<(32-cidr)) # Donne le masque à partir du /CIDR
 
 assert get_pointed_mask(13) == '255.248.0.0'
 get_pointed_mask(13)
+
+def get_int_cidr(mask):
+    """
+    pointed_mask : le masque sous forme d'une adresse ip décimale pointée
+    retourne le nombre d'adresses ip possibles
+    """
+    intmask = get_int_ip(mask) # Transforme le masque en entier
+    return bin(intmask).count("1") # retourne le CIDR en comptant le nombre de 1
+
+assert get_int_cidr('255.248.0.0') == 13
+get_int_cidr('255.248.0.0')
+
+def get_network_address(ip_cidr):
+    """
+    paramètre : ip_cidr une addresse ip décimale pointée avec son cidr exemple 192.168.1.40/24
+    Retourne, sous forme d'adresse IP pointée, l'adresse du réseau
+    """
+    ip , cidr = ip_cidr.split("/") # divise l'adresse ip du cidr
+    intip = get_int_ip(ip) # transforme en entier l'adresse ip
+    mask = get_pointed_mask(int(cidr)) # transforme en entier le cidr
+    intmask = get_int_ip(mask) # Donne le masque à partir du CIDR
+    network_address = intip & intmask # Fait un "ET" entre le masque et l'adresse ip pour avoir l'ip du réseau
+    return get_pointed_ip(network_address) #retourne l'adresse ip du réseau
+
+
+assert get_network_address('192.168.1.40/13') == '192.168.0.0'
+get_network_address('192.168.1.40/13')    
+    
+
+# def get_nb_ip(pointed_mask):
+#     """
+#     pointed_mask : le masque sous forme d'une adresse ip décimale pointée
+#     retourne le nombre d'adresses ip possibles
+#     """
+#     intip = get_int_ip(pointed_mask)
+#     binip = bin(intip)[2:]
+#     notbin = 
+#     
+    
+    
+    
